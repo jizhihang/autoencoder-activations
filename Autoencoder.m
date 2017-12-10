9facdb3ca621b0d9582cfb79a956dd022d7d9c2c
Aaron Levin <awlevin@comcast.net>
2017-12-10 15:01:36 -0600

    added gitignore and reorganized MNIST dataset into its own directory

--- Autoencoder.m
+++ Autoencoder.m
@@ -1,7 +1,8 @@
 %% CS 532 Final Project -- Rita Roloff, Justin Essert, Aaron Levin
 
-train_images = loadMNISTImages('train-images-idx3-ubyte');
-train_labels = loadMNISTLabels('train-labels-idx1-ubyte');
+train_images = loadMNISTImages('MNIST/train-images-idx3-ubyte');
+train_labels = loadMNISTLabels('MNIST/train-labels-idx1-ubyte');
 
-test_images = loadMNISTImages('t10k-images-idx3-ubyte');
-test_labels = loadMNISTLabels('t10k-labels-idx1-ubyte');
+test_images = loadMNISTImages('MNIST/t10k-images-idx3-ubyte');
+test_labels = loadMNISTLabels('MNIST/t10k-labels-idx1-ubyte');
+
