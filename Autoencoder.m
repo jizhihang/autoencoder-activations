466006c114df43464cfa81962540e3b91ad255ad
Aaron Levin <awlevin@comcast.net>
2017-12-10 14:58:56 -0600

    added MNIST dT dataset and code to load it into MATLAB. starting from scratch without tensorflow or python

---
+++ Autoencoder.m
@@ -0,0 +1,7 @@
+%% CS 532 Final Project -- Rita Roloff, Justin Essert, Aaron Levin
+
+train_images = loadMNISTImages('train-images-idx3-ubyte');
+train_labels = loadMNISTLabels('train-labels-idx1-ubyte');
+
+test_images = loadMNISTImages('t10k-images-idx3-ubyte');
+test_labels = loadMNISTLabels('t10k-labels-idx1-ubyte');
