999d1eb384c7dc484be3b6b35801966df263b83f
Aaron Levin <awlevin@comcast.net>
2017-12-10 15:08:39 -0600

    removed .DS_Store, was still lingering around after adding gitignore

--- Autoencoder.m
+++ Autoencoder.m
@@ -6,3 +6,4 @@ train_labels = loadMNISTLabels('MNIST/train-labels-idx1-ubyte');
 test_images = loadMNISTImages('MNIST/t10k-images-idx3-ubyte');
 test_labels = loadMNISTLabels('MNIST/t10k-labels-idx1-ubyte');
 
+
